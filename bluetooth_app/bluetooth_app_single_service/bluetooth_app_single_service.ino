d74247501a472856ec6d2c5ba262e6bc79883a55
elena <elena.rosato@mail.polimi.it>
2024-12-19 12:28:56 +0100

    -INDICATE al posto di Notify

--- bluetooth_app/bluetooth_app_single_service/bluetooth_app_single_service.ino
+++ bluetooth_app/bluetooth_app_single_service/bluetooth_app_single_service.ino
@@ -1,32 +1,28 @@
 #include <bluefruit.h> // Include la libreria Bluefruit per il modulo nRF52
 
-// Definisci i UUID per i servizi e le caratteristiche
-#define PROXIMITY_SERVICE_UUID    0x180F  // Servizio di prossimità
-//#define FALL_SERVICE_UUID         0x1811  // Servizio di caduta
+// Definisci i UUID per iL serviziO e le caratteristiche
+#define CASCHETTO_SERVICE_UUID    0x180F  // Servizio di prossimità
 #define PROXIMITY_CHAR_UUID       0x2A56  // Caratteristica di prossimità
 #define FALL_CHAR_UUID            0x2A58  // Caratteristica di caduta
 #define DISTRACTION_CHAR_UUID            0x2A54  // Caratteristica di Distrazione
 
 
 // Crea il servizio GATT per il rilevamento della macchina e della caduta
-BLEService proximityService(PROXIMITY_SERVICE_UUID);
+BLEService proximityService(CASCHETTO_SERVICE_UUID);
 //BLEService fallService(FALL_SERVICE_UUID);
 
 // Crea le caratteristiche per la prossimità (alert macchina) e la caduta
-BLECharacteristic proximityCharacteristic(PROXIMITY_CHAR_UUID, BLERead | BLENotify, 20); // max 20 byte
-BLECharacteristic fallCharacteristic(FALL_CHAR_UUID, BLERead | BLENotify, 20); // max 20 byte
-BLECharacteristic distractionCharacteristic(DISTRACTION_CHAR_UUID, BLERead | BLENotify, 20); // max 20 byte
+BLECharacteristic proximityCharacteristic(PROXIMITY_CHAR_UUID, BLERead | BLEIndicate, 20); // max 20 byte
+BLECharacteristic fallCharacteristic(FALL_CHAR_UUID, BLERead | BLEIndicate, 20); // max 20 byte
+BLECharacteristic distractionCharacteristic(DISTRACTION_CHAR_UUID, BLERead | BLEIndicate, 20); // max 20 byte
 
 unsigned long lastProximityAlertTime = 0;
-unsigned long lastFallAlertTime = 0;
-unsigned long lastDistractionAlertTime = 0;
 
 unsigned int connession=0;
 
 void setup() {
   Serial.begin(115200);
   while ( !Serial ) delay(10);   // for nrf52840 with native usb
-
 
 // Inizializza il Bluetooth
   Serial.println("Initialise the Bluefruit nRF52 module");
@@ -37,35 +33,26 @@ void setup() {
   Bluefruit.Periph.setConnectCallback(connect_callback);
   Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
 
-  
   proximityService.begin();
-  proximityCharacteristic.setProperties(CHR_PROPS_NOTIFY);
+  proximityCharacteristic.setProperties(CHR_PROPS_INDICATE);
   proximityCharacteristic.begin();
 
   //fallService.begin();
-  fallCharacteristic.setProperties(CHR_PROPS_NOTIFY);
+  fallCharacteristic.setProperties(CHR_PROPS_INDICATE);
   fallCharacteristic.begin();
 
-  distractionCharacteristic.setProperties(CHR_PROPS_NOTIFY);
+  distractionCharacteristic.setProperties(CHR_PROPS_INDICATE);
   distractionCharacteristic.begin();
-  
-  /*// Aggiungi i servizi e le caratteristiche
-  proximityService.addCharacteristic(proximityCharacteristic);
-  fallService.addCharacteristic(fallCharacteristic);
-*/
-  // Inizializza il dispositivo BLE come server
-  
-  Bluefruit.Advertising.addService(proximityService);
 
-  // Opzionalmente aggiungi il nome del dispositivo e altre informazioni
-  Bluefruit.Advertising.addName();
-  
-  // Avvia l'advertising
+  // Inizializza il dispositivo BLE come server
+  Bluefruit.Advertising.addService(proximityService);
+  Bluefruit.Advertising.addName();  // aggiungi il nome del dispositivo  
   //Bluefruit.Advertising.restartOnDisconnect(true);  // Continua a fare advertising dopo la disconnessione
   //Bluefruit.Advertising.setInterval(32, 244);       // Intervalli di advertising
   //Bluefruit.Advertising.setFastTimeout(30);         // Tempo massimo in modalità veloce
   //Bluefruit.Advertising.start(0);
-  Bluefruit.Advertising.start();
+  Bluefruit.Advertising.start();  // Avvia l'advertising
+
   
 
   Serial.println("Inizializzazione completata. Attendo connessione...");
@@ -75,17 +62,14 @@ void loop() {
   unsigned long currentMillis = millis();
   if(connession){
     
-
   // Simula un alert macchina ogni 3 secondi
   if (currentMillis - lastProximityAlertTime >= 9000) {
-    
       sendFallAlert("Caduta rilevata");
       delay(2000);
 
       sendProximityAlert("Macchina in avvicinamento");
       delay(3000);
       sendDistractionAlert("Sei distratto");
-
 
     lastProximityAlertTime = currentMillis;
   }
@@ -98,20 +82,20 @@ void loop() {
 void sendProximityAlert(const char* message) {
   Serial.println("Alert macchina: " + String(message));
   //proximityCharacteristic.write(message);  // Imposta il valore della caratteristica
-  proximityCharacteristic.notify(message);           // Invia la notifica
+  proximityCharacteristic.indicate(message);           // Invia la notifica
 }
 
 // Funzione per inviare un alert caduta
 void sendFallAlert(const char* message) {
   Serial.println("Alert caduta: " + String(message));
   //fallCharacteristic.write(message);  // Imposta il valore della caratteristica
-  fallCharacteristic.notify(message);           // Invia la notifica
+  fallCharacteristic.indicate(message);           // Invia la notifica
 }
 // Funzione per inviare un alert distrazione
 void sendDistractionAlert(const char* message) {
   Serial.println("Alert distrazione: " + String(message));
   //fallCharacteristic.write(message);  // Imposta il valore della caratteristica
-  distractionCharacteristic.notify(message);           // Invia la notifica
+  distractionCharacteristic.indicate(message);           // Invia la notifica
 }
 
 //todo: sto cercando di confrontare per capire se questo centra... o è il centrale???
